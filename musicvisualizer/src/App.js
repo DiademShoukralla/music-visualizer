551027a5a54a2d17091c0083c9bce96ccd53dc77
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2020-07-07 15:12:27 -0400

    Modified UI to have the toggle layer

--- musicvisualizer/src/App.js
+++ musicvisualizer/src/App.js
@@ -1,100 +1,37 @@
 import React, { useState } from 'react';
 import { 
   Box, 
-  Button, 
-  Collapsible, 
-  Heading, 
   Grommet,
+  Keyboard,
   Layer,
-  ResponsiveContext
+  Main
 } from 'grommet';
-import { FormClose, Notification } from 'grommet-icons';
 
-const theme = {
-  global: {
-    colors: {
-      brand: '#228BE6',
-    },
-    font: {
-      family: 'Roboto',
-      size: '18px',
-      height: '20px',
-    },
-  },
-};
+const ToggleLayer = (props) => (
+  (props.show ? <Layer {...props} full/> : null)
+);
 
-const AppBar = (props) => (
+const CanvasLayer = (props) => (
   <Box
-    tag='header'
-    direction='row'
-    align='center'
-    justify='between'
-    background='brand'
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation='medium'
-    style={{ zIndex: '1' }}
-    {...props}
-    />
+    {...props} fill/>
 );
 
 function App() {
-  const [showSidebar, setShowSidebar] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
   return (
-    <Grommet theme={theme} full>
-      <ResponsiveContext.Consumer>
-        {size => (
-          <Box fill>
-            <AppBar>
-              <Heading level='3' margin='none'>My App</Heading>
-              <Button 
-                icon={<Notification />} 
-                onClick={() => {setShowSidebar(!showSidebar)}} 
-              />
-            </AppBar>
-            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-              <Box flex align='center' justify='center'>
-                app body
-              </Box>
-              {(!showSidebar || size !== 'small') ? (
-                <Collapsible direction="horizontal" open={showSidebar}>
-                  <Box
-                    width='medium'
-                    background='light-2'
-                    elevation='small'
-                    align='center'
-                    justify='center'
-                  >
-                    sidebar
-                  </Box>
-                </Collapsible>
-              ): (
-                <Layer>
-                  <Box
-                    background='light-2'
-                    tag='header'
-                    justify='end'
-                    align='center'
-                    direction='row'
-                  >
-                    <Button
-                      icon={<FormClose />}
-                      onClick={() => setShowSidebar(false)}
-                    />
-                  </Box>
-                  <Box
-                    fill
-                    background='light-2'
-                    align='center'
-                    justify='center'
-                  >
-                    sidebar
-                  </Box>
-                </Layer>
-              )}
-            </Box>
-          </Box>
-        )}
-      </ResponsiveContext.Consumer>
+    <Grommet full>
+      <Keyboard 
+        target="document"
+        onEsc={() => {setShowMenu(!showMenu)}}>
+        <Main fill>
+          <CanvasLayer background="neutral-1">
+            Canvas
+          </CanvasLayer>
+          <ToggleLayer show={showMenu}>
+            Toggle Layer
+          </ToggleLayer>
+        </Main>
+      </Keyboard>
     </Grommet>
   );
 }
